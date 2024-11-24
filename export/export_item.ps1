d5cc2345a722ef22efef819cf92bb5cb752ba763
raditsoic <ruuoreo.o@gmail.com>
2024-11-24 18:04:57 +0700

    feat: export transaction & items from database

---
+++ export/export_item.ps1
@@ -0,0 +1,30 @@
+# Variables
+$ContainerName = "telkom-sms-psql-1"
+$DbUser = "soic"
+$DbName = "telkom_storage"
+$ExportPath = ".\"
+$OutputFile = "$ExportPath\items.csv"
+
+# SQL Query
+$query = @"
+COPY (
+    SELECT
+        i.id AS item_id,
+        i.name AS item_name,
+        c.name AS category_name,
+        i.quantity,
+        i.shelf
+    FROM items i
+    LEFT JOIN categories c ON i.category_id = c.id
+) TO STDOUT WITH CSV HEADER;
+"@
+
+# Execute the Query in Docker
+docker exec -i $ContainerName psql -U $DbUser -d $DbName -c $query | Out-File -FilePath $OutputFile -Encoding utf8
+
+# Check if the Export Succeeded
+if ($?) {
+    Write-Host "Export complete. File saved to $OutputFile"
+} else {
+    Write-Host "Export failed."
+}
